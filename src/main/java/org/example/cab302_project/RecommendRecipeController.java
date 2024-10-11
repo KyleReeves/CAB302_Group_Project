d02639c1a8354ef495e407ad3b6621fc80fb9479
idrcmatre <vaishnavrai9331@gmail.com>
2024-10-11 15:15:56 +1000

    Fixed bug

--- src/main/java/org/example/cab302_project/RecommendRecipeController.java
+++ src/main/java/org/example/cab302_project/RecommendRecipeController.java
@@ -4,30 +4,52 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class RecommendRecipeController {
-
-    private RecipeDAO recipeDAO = new RecipeDAO();
-    public static ObservableList<Recipe> RecipeList = FXCollections.observableArrayList();
+public class RecommendRecipeController implements Initializable {
 
     @FXML
     private Button backButton;
 
     @FXML
-    private TableView<Recipe> RecipeTable;
+    private TableView<Recipe> recipesTable;
 
-    @FXML
-    private TableColumn<Recipe, String> Recipe;
+    private RecipeDAO recipeDAO;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        recipeDAO = new RecipeDAO();
+        setupRecipesTable();
+        loadRecipes();
+    }
+
+    private void setupRecipesTable() {
+        TableColumn<Recipe, Integer> idColumn = new TableColumn<>("ID");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Recipe, String> nameColumn = new TableColumn<>("Recipe Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        recipesTable.getColumns().addAll(idColumn, nameColumn);
+    }
+
+    private void loadRecipes() {
+        List<Recipe> recipes = recipeDAO.getAll();
+        ObservableList<Recipe> observableRecipes = FXCollections.observableArrayList(recipes);
+        recipesTable.setItems(observableRecipes);
+    }
 
     @FXML
     protected void backButton() throws IOException {
@@ -35,33 +57,8 @@ public class RecommendRecipeController {
         FXMLLoader fxmlLoader = new FXMLLoader(IngredientTrackerApplication.class.getResource("menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), IngredientTrackerApplication.WIDTH, IngredientTrackerApplication.HEIGHT);
 
-        // Add stylesheet to the new scene
         scene.getStylesheets().add(Objects.requireNonNull(IngredientTrackerApplication.class.getResource("FormStyles.css")).toExternalForm());
         stage.setTitle("Ingredient Tracker");
         stage.setScene(scene);
-    }
-
-    // Initialize method: Set up table columns and load data
-    @FXML
-    public void initialize() {
-        RecipeTable = new TableView<Recipe>();
-        Recipe.setCellValueFactory(new PropertyValueFactory<>("Recipe"));
-        loadRecipes();
-    }
-
-    public void loadRecipes() {
-        try {
-            RecipeList.clear();
-            List<Recipe> recipes = recipeDAO.recomendedRecipes();
-            System.out.println("Loaded " + recipes.size() + " ingredients from database");
-            RecipeList.addAll(recipes);
-            RecipeTable.setItems(RecipeList);
-            System.out.println("Table items set, size: " + RecipeTable.getItems().size());
-        } catch (Exception e) {
-            System.err.println("Error loading ingredients: " + e.getMessage());
-            e.printStackTrace();
-            //showError("Error while loading ingredients", e.getMessage());
-        }
-        RecipeTable.refresh();
     }
 }
